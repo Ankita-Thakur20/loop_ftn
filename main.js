02d7a837212e13597964573075b030a4ce900442
Ankita-Thakur20 <ankitathakur036238@gmail.com>
2024-10-06 10:50:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// for (initialization; condition; increment/decrement) {
+//   // code to be executed
+// }
+
+
+let numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === 0) {
+//     console.log(i);
+//   }
+// }
